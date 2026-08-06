7a80374ab4bd9a857b446c7b5d011f155a53cc20
YIYUE <162600322+loveyouone@users.noreply.github.com>
2026-08-06 16:56:42 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -5273,14 +5273,15 @@ ac("UICorner",{
 CornerRadius=UDim.new(1,0)
 }),
 ac("UIStroke",{
-Thickness=1,
-ApplyStrokeMode="Border",
-Color=Color3.new(1,1,1),
-Transparency=0,
-},{
+    Thickness=1.5,
+    ApplyStrokeMode="Border",
+    Color=Color3.fromRGB(255,255,255),
+    Transparency=0.5,
+},
 ac("UIGradient",{
-Color=ColorSequence.new(Color3.fromHex"40c9ff",Color3.fromHex"e81cff")
-})
+    Color=ColorSequence.new(Color3.fromRGB(255,255,255),Color3.fromRGB(200,220,255)),
+    Transparency=NumberSequence.new{NumberSequenceKeypoint.new(0,0.4),NumberSequenceKeypoint.new(0.5,0.2),NumberSequenceKeypoint.new(1,0.4)}
+}),
 }),
 aj,
 ak,
